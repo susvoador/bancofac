3540165333711b9c5617fd1677bad713c21713b4
susvoador <joaoalbertomartinsbb@gmail.com>
2023-11-24 18:38:21 -0300

    poucas coisas a mais da autenticação

--- bancofac/src/app/app.module.ts
+++ bancofac/src/app/app.module.ts
@@ -1,19 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { environment } from '../environments/environment';
 import {provideFirebaseApp, initializeApp } from '@angular/fire/app'; 
 import { getFirestore, provideFirestore} from '@angular/fire/firestore';
+import { provideAuth, getAuth} from '@angular/fire/auth'; 
+import { provideStorage, getStorage} from '@angular/fire/storage';
+
+
+
+
+
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
      provideFirebaseApp(() => initializeApp(environment.firebase)),
-     provideFirestore(() => getFirestore())
+     provideFirestore(() => getFirestore()),
+     provideAuth(() => getAuth()),
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
