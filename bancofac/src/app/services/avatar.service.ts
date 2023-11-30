dd1f3218db7af055e092ba64d034394f79568358
susvoador <joaoalbertomartinsbb@gmail.com>
2023-11-30 15:32:22 -0300

    mais algumas poucas mudanças

--- bancofac/src/app/services/avatar.service.ts
+++ bancofac/src/app/services/avatar.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Auth } from '@angular/fire/auth';
+import { Firestore, doc, docData } from '@angular/fire/firestore';
+import { Storage } from '@angular/fire/storage';
+import { Photo } from '@capacitor/camera'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class AvatarService {
 
-  constructor() { }
+
+  constructor(
+    private auth: Auth, 
+    private firestore: Firestore,
+    private storage: Storage
+  ) {}
+
+  getUserProfile() {
+     const user = this.auth.currentUser;
+     const userDocRef = doc(this.firestore, `users/${user.uid}`)
+  }
+
+  async uploadImage(cameraFile: Photo) {}
+  
 }
